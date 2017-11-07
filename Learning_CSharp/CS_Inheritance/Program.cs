569fd0abfd0a520304d78959b3841a16b668f944
Hoang Viet <davishoang96@gmail.com>
2017-11-07 22:46:43 +0700

    Polymorphism

--- Learning_CSharp/CS_Inheritance/Program.cs
+++ Learning_CSharp/CS_Inheritance/Program.cs
@@ -10,6 +10,52 @@ namespace CS_Inheritance
     {
         static void Main(string[] args)
         {
+            ChildClass child = new ChildClass();
+
+            child.print();
+
+            ((giantClass)child).print();
+        }
+    }
+
+    public class giantClass
+    {
+        string giantString;
+        public giantClass()
+        {
+            Console.WriteLine();
+        }
+
+        public giantClass(string myString)
+        {
+            giantString = myString;
+            Console.WriteLine(giantString);
+        }
+
+        public void print()
+        {
+            Console.WriteLine("This is a giant class");
+        }
+    }
+
+    public class ParentClass : giantClass
+    {
+        public ParentClass() : base("From Derived")
+        {
+            Console.WriteLine("Parent Constructor.");
+        }
+
+        public void prints()
+        {
+            Console.WriteLine("I'm a Parent Class.");
+        }
+    }
+
+    public class ChildClass : ParentClass 
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("Child Constructor.");
         }
     }
 }
---
+++ Learning_CSharp/CS_Polymorphism/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawObject[] newobj = new DrawObject[3];
+            newobj[0] = new line();
+            newobj[1] = new Circle();
+            newobj[2] = new Triangle();
+
+            foreach(DrawObject drawObj in newobj)
+            {
+                drawObj.Draw();
+            }
+        }
+
+        public class DrawObject
+        {
+            public virtual void Draw()
+            {
+                Console.WriteLine("Draw something");
+            }
+        }
+
+        public class line : DrawObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I draw a line");
+            }
+        }
+
+        public class Circle : DrawObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I draw a circle");
+            }
+        }
+
+        public class Triangle : DrawObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I draw a triangle");
+            }
+        }
+
+    }
+}
